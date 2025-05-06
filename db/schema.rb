e785866840577a7490cec999073100ff06a6624e
Sandrine Halbronn sandhales@gmail.com <sandhales@gmail.com>
2025-05-06 17:06:06 +0200

    Change movie length format

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_06_140715) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_06_150327) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.text "summary"
-    t.string "length"
+    t.integer "length"
     t.date "release_date"
     t.string "genre"
     t.string "director"
