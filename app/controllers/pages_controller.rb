c006d385a027a3595d4a3db0f68cf0acaf42360c
Sandrine Halbronn sandhales@gmail.com <sandhales@gmail.com>
2025-05-12 20:02:30 +0200

    Create pages controller and view and simple api call

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,22 @@
+require "open-uri"
+
 class PagesController < ApplicationController
+
+  def home
+    generate_movie
+  end
+
+  private
+
+  def generate_movie
+    url = "https://api.themoviedb.org/3/discover/movie?api_key=#{ENV['TMDB_KEY']}&region=FR&language=fr-FR"
+    response = JSON.parse(URI.open(url).read)
+
+    if response["results"].present?
+      movies = response["results"]
+      @movie = movies.sample
+      @movie_title = @movie['title']
+      @movie_summary = @movie['overview']
+    end
+  end
 end
