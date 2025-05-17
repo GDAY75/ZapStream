713e69e4c32500cd7deb92d41ac5e43a1f7d9d1d
Gil DAYAN <gil.dayan@orange.fr>
2025-05-18 00:04:44 +0200

    Ajout du nom du réalisateur

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -26,11 +26,16 @@ class PagesController < ApplicationController
 
   def generate_movie_details(movie)
     movie_id = movie["id"]
-    url = "https://api.themoviedb.org/3/movie/#{movie_id}?api_key=#{ENV['TMDB_KEY']}&language=fr-FR"
-    response = JSON.parse(URI.open(url).read)
+    details_url = "https://api.themoviedb.org/3/movie/#{movie_id}?api_key=#{ENV['TMDB_KEY']}&language=fr-FR"
+    credits_url = "https://api.themoviedb.org/3/movie/#{movie_id}/credits?api_key=#{ENV['TMDB_KEY']}"
 
-    if response["runtime"].present?
-      @movie_length = response["runtime"]
+    details = JSON.parse(URI.open(details_url).read)
+    credits = JSON.parse(URI.open(credits_url).read)
+
+    if details["runtime"].present?
+      @movie_length = details["runtime"]
+      director = credits["crew"].find { |person| person["job"] == "Director" }
+      @movie_director = director ? director["name"] : "Inconnu"
 
       if @movie_length >= 60
         @hours = @movie_length / 60
