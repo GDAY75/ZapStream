5c57c4b2b7c79caff9306040d2916c6ac4162b8e
Gil DAYAN <gil.dayan@orange.fr>
2025-05-21 19:32:46 +0200

    La méthode de madame est avancée

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,8 @@ class PagesController < ApplicationController
   def home
     generate_movie
     generate_movie_details(@movie)
+    generate_movie_providers(@movie)
+    generate_movie_credits(@movie)
   end
 
   private
@@ -19,6 +21,7 @@ class PagesController < ApplicationController
       @movie_title = @movie['title']
       @movie_summary = @movie['overview']
       @movie_release_date = @movie['release_date'].first(4)
+      @movie_poster_url = "https://image.tmdb.org/t/p/w300#{@movie['poster_path']}"
     end
 
     return @movie
@@ -26,17 +29,12 @@ class PagesController < ApplicationController
 
   def generate_movie_details(movie)
     movie_id = movie["id"]
-    details_url = "https://api.themoviedb.org/3/movie/#{movie_id}?api_key=#{ENV['TMDB_KEY']}&language=fr-FR"
-    credits_url = "https://api.themoviedb.org/3/movie/#{movie_id}/credits?api_key=#{ENV['TMDB_KEY']}"
+    url = "https://api.themoviedb.org/3/movie/#{movie_id}?api_key=#{ENV['TMDB_KEY']}&language=fr-FR"
+    response = JSON.parse(URI.open(url).read)
 
-    details = JSON.parse(URI.open(details_url).read)
-    credits = JSON.parse(URI.open(credits_url).read)
-
-    if details["runtime"].present?
-      @movie_length = details["runtime"]
-      director = credits["crew"].find { |person| person["job"] == "Director" }
-      @movie_director = director ? director["name"] : "Inconnu"
-      @movie_categories = details["genres"]
+    if response["runtime"].present?
+      @movie_length = response["runtime"]
+      @movie_categories = response["genres"]
 
       if @movie_length >= 60
         @hours = @movie_length / 60
@@ -44,6 +42,27 @@ class PagesController < ApplicationController
       else
         @min = @movie_length
       end
+    end
+  end
+
+  def generate_movie_providers(movie)
+    movie_id = movie["id"]
+    url = "https://api.themoviedb.org/3/movie/#{movie_id}/watch/providers?api_key=#{ENV['TMDB_KEY']}"
+    response = JSON.parse(URI.open(url).read)
+
+    if response["results"].present? && response["results"]["FR"].present? && response["results"]["FR"]["flatrate"].present?
+      @movie_provider = response["results"]["FR"]["flatrate"][0]["provider_name"]
+    end
+  end
+
+  def generate_movie_credits(movie)
+    movie_id = movie["id"]
+    url = "https://api.themoviedb.org/3/movie/#{movie_id}/credits?api_key=#{ENV['TMDB_KEY']}"
+    response = JSON.parse(URI.open(url).read)
+
+    if response["crew"].present?
+      director = response["crew"].find { |person| person["job"] == "Director" }
+      @movie_director = director ? director["name"] : "Inconnu"
     end
   end
 end
