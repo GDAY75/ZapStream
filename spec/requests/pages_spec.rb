9e7aefaacc1c6c845cb58699479526d570fa4260
Sandrine Halbronn sandhales@gmail.com <sandhales@gmail.com>
2025-05-21 21:04:03 +0200

    Add movie director rspec

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -2,11 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "Pages", type: :request do
   describe "GET /home" do
-    it "affiche une image de poster sur la page" do
+
+    it "displays a movie poster on the home page" do
       get root_path
 
       expect(response).to have_http_status(:success)
       expect(response.body).to include("https://image.tmdb.org/t/p/w")
+    end
+
+    it "displays the name of the movie director"
+      get root_path
+      expect(response.body).to match(/Réalisateur :\s*.+/)
     end
   end
 end
